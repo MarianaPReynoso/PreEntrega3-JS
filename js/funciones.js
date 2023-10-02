fa27d6ba7bca19f18951e038b334f48dd88d5caa
MarianaPReynoso <reynosopmariana@gmail.com>
2023-10-02 19:57:38 -0300

    Arreglados algunos detalles

--- js/funciones.js
+++ js/funciones.js
@@ -1,26 +1,11 @@
 function validarMonto () {
     let recordatorio = document.getElementById("recordatorio");
     var lista = document.getElementById("prestamos").value;
-    document.getElementById("recordatorio").innerHTML = "Recuerde que el valor máximo del " + lista;
+    document.getElementById("recordatorio").innerHTML = "Se ha excedido del valor máximo para el " + lista;
     recordatorio.className = "text-danger"
 } 
 
 function Prestamo (nombre, montoMaximo) {
     this.nombre = nombre;
     this.monto = montoMaximo;
-}
-
-function cuotas(){
-    let cantidadCuotas /* = parseInt(prompt("Indique la cantidad de cuotas (12/24/36):")); */
-
-    /* while(cantidadCuotas != 12 && cantidadCuotas != 24 && cantidadCuotas != 36){
-        alert("Por favor, ingrese la cantidad de cuotas permitidas");
-        cantidadCuotas = parseInt(prompt("Indique la cantidad de cuotas (12/24/36):"));
-
-        if (cantidadCuotas == 12 && cantidadCuotas == 24 && cantidadCuotas == 36){
-            break;
-        }
-    } */
-
-    return cantidadCuotas;
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const pidieronPrestamo = [
-    {id: 1, alias: "Virginia Castro", tipo: "Préstamo Simple", numCuotas: 24},
+    {id: 1, alias: "Virginia Castro", tipo: "Préstamo Básico", numCuotas: 24},
     {id: 2, alias: "Fernando Arabia", tipo: "Préstamo Premium", numCuotas: 36},
     {id: 3, alias: "Josefina Perez", tipo: "Préstamo Premium", numCuotas: 36},
     {id: 4, alias: "Alma Zulema", tipo: "Préstamo Simple", numCuotas: 12},
@@ -8,12 +8,18 @@ const pidieronPrestamo = [
 
 const ingresonWeb = ["Virginia Castro", "Fernando Arabia", "Marisol Lopez", "José Guzman", "Josefina Perez", "Jimena Galli", "Alma Zulema", "Oliver Gomez"];
 
-/* let nombre = prompt("Ingrese su nombre:").toUpperCase();
-let apellido = prompt ("Ingrese su apellido:").toUpperCase();
-let nombreCompleto = nombre + " " + apellido;
-alert ("¡Bienvenido! " + nombreCompleto) */
+const validarNombre = () => {
+    let nombreCompleto = document.getElementById("nombreCompleto").value;
+    let aviso = document.getElementById("aviso");
 
-/* let pedirPrestamo = prompt("¿Le gustaría solicitar un préstamo con nosotros? (Si, No)").toUpperCase(); */
+    if (nombreCompleto == "") {
+        aviso.innerHTML = "Por favor indique su nombre para continuar";
+        aviso.className = "text-danger";
+        return false;
+    }
+}
+
+document.getElementById("nombreCompleto").addEventListener("input", validarNombre);
 
 document.getElementById("opciones").addEventListener("change", function(e) {
     if(this.value == "1") {
@@ -22,8 +28,6 @@ document.getElementById("opciones").addEventListener("change", function(e) {
         document.getElementById("cantCuotas").innerHTML = document.getElementById("numeroCuotas").innerHTML;
     }
 })
-
-let valorPrestamo;
 
 const TNA = 138;
 const TEA = 268;
@@ -34,15 +38,14 @@ const tipoPrestamo2 = new Prestamo ("Préstamo Medio", 2000000);
 const tipoPrestamo3 = new Prestamo ("Préstamo Premium", 3000000);
 let interesCuota; 
 
-let cantidadCuotas = cuotas();
+let formularioPrestamo = document.getElementById("formularioPrestamo");
 
-const validarCuotas = (event) => {
-    event.preventDefault();
+const validarCuotas = () => {
     let opcionCuota = document.getElementById("opcionCuota").value;
+    let valorPrestamo = parseInt(document.getElementById("saldoPrestamo").value);
     let resultado2 = document.getElementById("resultado2");
-    let formularioPrestamo = document.getElementById("formularioPrestamo");
 
-    if(opcionCuota.value == "12"){
+    if(opcionCuota == "12"){
         resultado2.innerHTML = interesCuota = 30;
     } else if (opcionCuota.value == "24"){
         interesCuota = 40;
@@ -50,11 +53,11 @@ const validarCuotas = (event) => {
         interesCuota = 50;
     }
 
-    let cuotaPura = valorPrestamo / cantidadCuotas;
-    let interesPrestamo = interesCuota / cantidadCuotas;
-    let tnaCuota = TNA / cantidadCuotas;
-    let teaCuota = TEA / cantidadCuotas;
-    let cfteaCuota = CFTEA / cantidadCuotas;
+    let cuotaPura = valorPrestamo / opcionCuota;
+    let interesPrestamo = interesCuota / opcionCuota;
+    let tnaCuota = TNA / opcionCuota;
+    let teaCuota = TEA / opcionCuota;
+    let cfteaCuota = CFTEA / opcionCuota;
 
     let interesPorCuota = (cuotaPura * interesPrestamo) / 100;
     let interesTnaCuota = (cuotaPura * tnaCuota) / 100;
@@ -63,61 +66,17 @@ const validarCuotas = (event) => {
 
     let valorCuota = cuotaPura + interesPorCuota + interesTnaCuota + interesTeaCuota + interesCfteaCuota;
 
-    resultado2.innerHTML = "Los datos se enviaron correctamente";
-    form
-    alert(`Su préstamo de $${valorPrestamo} será devuelto en ${cantidadCuotas} cuotas con un interés del ${interesCuota}%. Cada cuota tendrá un valor de $${valorCuota.toFixed(2)}`)
+    resultado2.innerHTML = "¡Los datos se enviaron correctamente!";
+    resultado2.className = "text-success";
+
+    alert(`Su préstamo de $${valorPrestamo} será devuelto en ${opcionCuota} cuotas con un interés del ${interesCuota}%. Cada cuota tendrá un valor de $${valorCuota.toFixed(2)}`)
     formularioPrestamo.onsubmit();
 }
 
-
-
-/* if (pedirPrestamo == "SI"){ */
-   /*  valorPrestamo = parseFloat(prompt("¡Genial! Indíquenos la suma a la cual quiere acceder (Máx: $"+ maximoSolicitado + ")"));
-        if (valorPrestamo <= 1000000){
-            alert(`Muy bien, a seleccionado el ${tipoPrestamo.nombre}, sus $${valorPrestamo} serán depositados en su cuenta bancaria`);
-        } else {
-            alert(`Muy bien, a seleccionado el ${tipoPrestamo2.nombre}, sus $${valorPrestamo} serán depositados en su cuenta bancaria`);
-        }
-
-    while(valorPrestamo > maximoSolicitado){
-        valorPrestamo = parseFloat(prompt("¡Genial! Indíquenos la suma a la cual quiere acceder(Máx: $"+ maximoSolicitado + ")"));
-
-        if (valorPrestamo <= maximoSolicitado){
-            break;
-        }
-    } */
-
-   /*  let cantidadCuotas = cuotas(); */
-
-    /* if(cantidadCuotas == 12){
-        interesCuota = 30;
-    } else if (cantidadCuotas == 24){
-        interesCuota = 40;
-    } else {
-        interesCuota = 50;
-    } */
-
-    /* let cuotaPura = valorPrestamo / cantidadCuotas;
-    let interesPrestamo = interesCuota / cantidadCuotas;
-    let tnaCuota = TNA / cantidadCuotas;
-    let teaCuota = TEA / cantidadCuotas;
-    let cfteaCuota = CFTEA / cantidadCuotas;
-
-    let interesPorCuota = (cuotaPura * interesPrestamo) / 100;
-    let interesTnaCuota = (cuotaPura * tnaCuota) / 100;
-    let interesTeaCuota = (cuotaPura * teaCuota) / 100;
-    let interesCfteaCuota = (cuotaPura * cfteaCuota) / 100;
-
-    let valorCuota = cuotaPura + interesPorCuota + interesTnaCuota + interesTeaCuota + interesCfteaCuota; */
-
-    /* alert(`Su préstamo de $${valorPrestamo} será devuelto en ${cantidadCuotas} cuotas con un interés del ${interesCuota}%. Cada cuota tendrá un valor de $${valorCuota.toFixed(2)}`) */
-
-    /* pidieronPrestamo.push(nombreCompleto);
-    console.log(pidieronPrestamo);
-}else{
-    alert("¡Lo invitamos a seguir navegando!");
-    ingresonWeb.push(nombreCompleto);
-} */
+formularioPrestamo.addEventListener("submit", (e) => {
+    e.preventDefault()
+    validarCuotas()
+});
 
 const numeDeCuotas = pidieronPrestamo.find(item => item.numCuotas == 12);
 console.log(numeDeCuotas);
